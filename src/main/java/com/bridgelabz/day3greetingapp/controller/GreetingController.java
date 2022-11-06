6ff2100dc30d475abff27458ae7563740e4a4d96
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-11-06 22:26:03 +0530

    Greeting App day3 problem

---
+++ src/main/java/com/bridgelabz/day3greetingapp/controller/GreetingController.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day3greetingapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+public class GreetingController {
+
+}
---
+++ src/test/java/com/bridgelabz/day3greetingapp/Day3GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day3greetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3GreetingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
