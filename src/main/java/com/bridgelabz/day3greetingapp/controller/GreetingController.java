4e1c1f20aaafdc347d58e1b749198b9deaea8a4d
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-11-06 22:50:26 +0530

    GreetingController return JSON for different HTTP methods UC1.

--- src/main/java/com/bridgelabz/day3greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/day3greetingapp/controller/GreetingController.java
@@ -1,7 +1,25 @@
 package com.bridgelabz.day3greetingapp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/greetings")
 public class GreetingController {
 
+    //UC1
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello From Bridgelabz";
+    }
+
+    @RequestMapping("/query/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHelloParam(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
 }
